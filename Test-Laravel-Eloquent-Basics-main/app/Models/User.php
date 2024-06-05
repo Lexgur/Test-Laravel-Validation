6d5b404cc523ff25b40d639f30aac971fef59608
Lexgur <edgaras.malukas@gmail.com>
2024-06-05 13:01:58 +0300

    Let's go

--- Test-Laravel-Eloquent-Basics-main/app/Models/User.php
+++ Test-Laravel-Eloquent-Basics-main/app/Models/User.php
@@ -8,6 +8,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @method static whereNotNull(string $string)
+ * @method static where(string $string, string $string1, $null)
+ * @method static findOrFail($userId)
+ * @method static firstOrCreate(array $array)
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
